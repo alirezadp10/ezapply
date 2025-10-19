f34e6cce787bbb3585a64d6cb7f9922f3e806249
alirezadp10 <alirezadp10@gmail.com>
2025-10-19 15:10:09 +0330

    fix an issue

--- bot/job_applicator.py
+++ bot/job_applicator.py
@@ -28,6 +28,10 @@ class JobApplicator:
                 answers = AIService.ask_form_answers(payload)
                 self.filler.fill_fields(payload, answers)
 
+            if self.driver.find_elements(By.CSS_SELECTOR, '[type="error-pebble-icon"]'):
+                self._close_and_next()
+                raise Exception("couldn't fill out the form.")
+
             if self._submit_if_ready(job_id):
                 break
 
@@ -54,3 +58,39 @@ class JobApplicator:
             return True
         except Exception:
             return False
+
+    def _close_and_next(self):
+        self._click_if_exists('[aria-label="Dismiss"]')
+        self._click_if_exists('[data-control-name="discard_application_confirm_btn"]')
+        time.sleep(settings.DELAY_TIME)
+
+
+
+
+
+    # def apply_to_job(self, job_id: int):
+    #     """Automate the Easy Apply process for a given job."""
+    #     logger.info(f"🟩 Applying to job: {job_id}")
+    #
+    #     self.open_job_posting(job_id)
+    #     self.click_apply_button()
+    #
+    #     while True:
+    #         if self.element_exists('[type="error-pebble-icon"]'):
+    #             self._close_and_next()
+    #             raise Exception("couldn't fill out the form.")
+    #
+    #         if self.submit_if_ready(job_id):
+    #             self.click_if_exists('[aria-label="Dismiss"]')
+    #             break
+    #
+    #         if self.handle_resume_step():
+    #             continue
+    #
+    #         payload = self.parse_form_fields()
+    #         if payload:
+    #             answers = self.ask_from_ai(payload)
+    #             self.fill_form_fields(payload, answers)
+    #
+    #         if self.next_step_or_review():
+    #             continue
