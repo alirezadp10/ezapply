1a9f265610a2dbd50c5c4a121608d69273438695
alirezadp10 <alirezadp10@gmail.com>
2025-10-19 13:20:51 +0330

    fix an issue

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from bot.logger_manager import setup_logger
 from bot.selenium_bot import SeleniumBot
 from bot.config import settings
+from bot.driver_manager import DriverManager
 
 def main():
     logger = setup_logger()
     logger.info("🚀 Bot is starting...")
+
     bot = SeleniumBot(name="JobApplierBot", db_url=settings.SQLITE_DB_PATH)
 
     try:
@@ -12,7 +14,9 @@ def main():
     except Exception as e:
         logger.exception(f"❌ Error while running bot: {e}")
     finally:
-        bot.kill_driver()
+        # Properly close WebDriver
+        if bot.driver:
+            DriverManager.close_driver(bot.driver)
         logger.info("🛑 Bot finished execution.")
 
 if __name__ == "__main__":
