76e57103dcb63285f6b28c75c710ebb57991dd23
alirezadp10 <alirezadp10@gmail.com>
2025-11-13 22:37:20 +0330

    fix an issue

--- bot/enums/modes_enum.py
+++ bot/enums/modes_enum.py
@@ -4,7 +4,6 @@ from enum import Enum
 class ModesEnum(str, Enum):
     EXPLORE = "explore"
     FETCH_QUESTIONS = "fetch questions"
-    SCORE = "score"
     APPLY = "apply"
 
     def __str__(self) -> str:
