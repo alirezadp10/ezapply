a4566d61b800fc1eca8009c0fdafc42345ab1e8d
alirezadp10 <alirezadp10@gmail.com>
2025-11-16 13:53:13 +0330

    fix some issues

--- bot/settings.py
+++ bot/settings.py
@@ -29,8 +29,8 @@ class Settings:
     )
     DEEPINFRA_API_KEY: str = os.getenv("DEEPINFRA_API_KEY")
 
-    OPENROUTER_BASE_URL: str = os.getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1")
-    OPENAI_MODEL_NAME: str = os.getenv("OPENAI_MODEL_NAME", "openai:nvidia/nemotron-nano-12b-v2-vl:free")
+    OPENAI_BASE_URL: str = os.getenv("OPENAI_BASE_URL", "https://openrouter.ai/api/v1")
+    OPENAI_MODEL_NAME: str = os.getenv("OPENAI_MODEL_NAME", "openai:openai/gpt-oss-20b:free")
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
 
     # AI
